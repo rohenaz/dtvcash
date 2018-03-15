32fcbe7a39e1f1eaf335833aefdc0e33dee28d91
Jason Chavannes <jason.chavannes@gmail.com>
2018-03-14 17:56:47 -0700

    If block height too high, redirect to most recent block.

--- web/server/block.go
+++ web/server/block.go
@@ -6,6 +6,7 @@ import (
 	"github.com/jchavannes/jgo/jerr"
 	"github.com/jchavannes/jgo/web"
 	"net/http"
+	"strconv"
 )
 
 var blockRoute = web.Route{
@@ -14,7 +15,16 @@ var blockRoute = web.Route{
 		height := r.Request.GetUrlNamedQueryVariableUInt(paramHeight.Id)
 		block, err := db.GetBlockByHeight(height)
 		if err != nil {
-			r.Error(jerr.Get("error getting block height", err), http.StatusUnprocessableEntity)
+			if ! db.IsRecordNotFoundError(err) {
+				r.Error(jerr.Get("error getting block by height", err), http.StatusUnprocessableEntity)
+				return
+			}
+			block, err = db.GetRecentBlock()
+			if err != nil {
+				r.Error(jerr.Get("error getting most recent block", err), http.StatusUnprocessableEntity)
+				return
+			}
+			r.SetRedirect(res.UrlBlockView + "/" + strconv.Itoa(int(block.Height)))
 			return
 		}
 		r.Helper["Block"] = block
