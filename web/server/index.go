8b9992124f8276f4f2871bec47e10c08343614d2
Jason Chavannes <jason.chavannes@gmail.com>
2018-04-10 12:10:11 -0700

    Display balance and address on dashboard

--- web/server/index.go
+++ web/server/index.go
@@ -28,6 +28,23 @@ var (
 				return
 			}
 			r.Helper["Key"] = key
+
+			transactions, err := db.GetTransactionsForKey(key.Id)
+			if err != nil {
+				r.Error(jerr.Get("error getting transactions for key", err), http.StatusInternalServerError)
+				return
+			}
+			var balance int64
+			var balanceBCH float64
+			for _, transaction := range transactions {
+				balance += transaction.GetValue()
+				balanceBCH += transaction.GetValueBCH()
+			}
+			for i, j := 0, len(transactions)-1; i < j; i, j = i+1, j-1 {
+				transactions[i], transactions[j] = transactions[j], transactions[i]
+			}
+			r.Helper["Balance"] = balance
+			r.Helper["BalanceBCH"] = balanceBCH
 			r.RenderTemplate("dashboard")
 		},
 	}
