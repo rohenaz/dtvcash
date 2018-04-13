065ba828c18daa8e4a4990c1b0139d7f11c63e3e
Jason Chavannes <jason.chavannes@gmail.com>
2018-04-12 23:16:36 -0700

    Organize memo and transaction saving

--- app/bitcoin/transaction/memo.go
+++ app/bitcoin/transaction/memo.go
@@ -11,116 +11,157 @@ import (
 	"html"
 )
 
-func FindAndSaveMemos(txn *db.Transaction, block *db.Block) error {
-	var pkHash []byte
-	for _, in := range txn.TxIn {
-		tmpPkHash := in.GetAddress().GetScriptAddress()
-		if len(tmpPkHash) > 0 {
-			pkHash = tmpPkHash
-		}
-	}
-	if len(pkHash) == 0 {
-		// Unknown script type
-		return nil
-	}
-	addressPkHash, err := btcutil.NewAddressPubKeyHash(pkHash, &wallet.MainNetParamsOld)
-	if err != nil {
-		return jerr.Get("error getting pubkeyhash from memo test", err)
-	}
-	address := addressPkHash.EncodeAddress()
-	txnHash := txn.GetChainHash().CloneBytes()
-	for _, out := range txn.TxOut {
-		if len(out.PkScript) < 5 || ! bytes.Equal(out.PkScript[0:3], []byte{
+func GetMemoOutputIfExists(txn *db.Transaction) (*db.TransactionOut, error) {
+	var out *db.TransactionOut
+	for _, txOut := range txn.TxOut {
+		if len(txOut.PkScript) < 5 || ! bytes.Equal(txOut.PkScript[0:3], []byte{
 			txscript.OP_RETURN,
 			txscript.OP_DATA_2,
 			memo.CodePrefix,
 		}) {
 			continue
 		}
-		// Save MemoTest
-		var test = db.MemoTest{
-			TxHash:   txnHash,
-			PkHash:   pkHash,
-			PkScript: out.PkScript,
-			Address:  address,
+		if out != nil {
+			return nil, jerr.New("UNEXPECTED ERROR: found more than one memo in transaction")
 		}
-		if block != nil {
-			test.BlockId = block.Id
-		}
-		err := test.Save()
+		out = txOut
+	}
+	return out, nil
+}
+
+func SaveMemo(txn *db.Transaction, out *db.TransactionOut, block *db.Block) error {
+	_, err := db.GetMemoTest(txn.Hash)
+	if err != nil && ! db.IsRecordNotFoundError(err) {
+		return jerr.Get("error getting memo_test", err)
+	}
+	if err == nil {
+		err = updateMemo(txn, out, block)
 		if err != nil {
-			if ! db.IsAlreadyExistsError(err) || block == nil {
-				return jerr.Get("error saving memo test", err)
-			}
-			memoTest, err := db.GetMemoTest(txnHash)
-			if err != nil {
-				return jerr.Get("error getting existing memo test", err)
-			}
-			memoTest.BlockId = block.Id
-			err = memoTest.Save()
-			if err != nil {
-				return jerr.Get("error saving existing memo test", err)
-			}
+			return jerr.Get("error updating memo", err)
 		}
-		switch out.PkScript[3] {
-		case memo.CodePost:
-			var post = db.MemoPost{
-				TxHash:   txnHash,
-				PkHash:   pkHash,
-				PkScript: out.PkScript,
-				Address:  address,
-				Message:  html.EscapeString(string(out.PkScript[5:])),
+	} else {
+		err = newMemo(txn, out, block)
+		if err != nil {
+			return jerr.Get("error saving new memo", err)
+		}
+	}
+	return nil
+}
+
+func getInputPkHash(txn *db.Transaction) (*btcutil.AddressPubKeyHash, error) {
+	var pkHash []byte
+	for _, in := range txn.TxIn {
+		tmpPkHash := in.GetAddress().GetScriptAddress()
+		if len(tmpPkHash) > 0 {
+			if len(pkHash) != 0 && ! bytes.Equal(tmpPkHash, pkHash) {
+				return nil, jerr.New("error found multiple addresses in inputs")
 			}
-			if len(txn.TxIn) == 1 {
-				post.ParentHash = txn.TxIn[0].PreviousOutPointHash
-			}
-			if block != nil {
-				post.BlockId = block.Id
-			}
-			err := post.Save()
-			if err != nil {
-				if ! db.IsAlreadyExistsError(err) || block == nil {
-					return jerr.Get("error saving memo post", err)
-				}
-				post, err := db.GetMemoTest(txnHash)
-				if err != nil {
-					return jerr.Get("error getting existing memo post", err)
-				}
-				post.BlockId = block.Id
-				err = post.Save()
-				if err != nil {
-					return jerr.Get("error saving existing memo post", err)
-				}
-			}
-		case memo.CodeSetName:
-			var setName = db.MemoSetName{
-				TxHash:   txnHash,
-				PkHash:   pkHash,
-				PkScript: out.PkScript,
-				Address:  address,
-				Name:     html.EscapeString(string(out.PkScript[5:])),
-			}
-			if len(txn.TxIn) == 1 {
-				setName.ParentHash = txn.TxIn[0].PreviousOutPointHash
-			}
-			if block != nil {
-				setName.BlockId = block.Id
-			}
-			err := setName.Save()
-			if err != nil {
-				if ! db.IsAlreadyExistsError(err) || block == nil {
-					return jerr.Get("error saving memo set name", err)
-				}
-				setName, err := db.GetMemoTest(txnHash)
-				if err != nil {
-					return jerr.Get("error getting existing memo set name", err)
-				}
-				setName.BlockId = block.Id
-				err = setName.Save()
-				if err != nil {
-					return jerr.Get("error saving existing memo set name", err)
-				}
-			}
+			pkHash = tmpPkHash
+		}
+	}
+	if len(pkHash) == 0 {
+		// Unknown script type
+		return nil, jerr.New("error no pk hash found")
+	}
+	addressPkHash, err := btcutil.NewAddressPubKeyHash(pkHash, &wallet.MainNetParamsOld)
+	if err != nil {
+		return nil, jerr.Get("error getting pubkeyhash from memo test", err)
+	}
+	return addressPkHash, nil
+}
+
+func newMemo(txn *db.Transaction, out *db.TransactionOut, block *db.Block) error {
+	inputAddress, err := getInputPkHash(txn)
+	if err != nil {
+		return jerr.Get("error getting pk hash from input", err)
+	}
+	var blockId uint
+	if block != nil {
+		blockId = block.Id
+	}
+	// Used for ordering
+	var parentHash []byte
+	if len(txn.TxIn) == 1 {
+		parentHash = txn.TxIn[0].PreviousOutPointHash
+	}
+	var memoTest = db.MemoTest{
+		TxHash:   txn.Hash,
+		PkHash:   inputAddress.ScriptAddress(),
+		PkScript: out.PkScript,
+		Address:  inputAddress.EncodeAddress(),
+		BlockId:  blockId,
+	}
+	err = memoTest.Save()
+	if err != nil {
+		return jerr.Get("error saving memo_test", err)
+	}
+	switch out.PkScript[3] {
+	case memo.CodePost:
+		var memoPost = db.MemoPost{
+			TxHash:     txn.Hash,
+			PkHash:     inputAddress.ScriptAddress(),
+			PkScript:   out.PkScript,
+			ParentHash: parentHash,
+			Address:    inputAddress.EncodeAddress(),
+			Message:    html.EscapeString(string(out.PkScript[5:])),
+			BlockId:    blockId,
+		}
+		err := memoPost.Save()
+		if err != nil {
+			return jerr.Get("error saving memo_post", err)
+		}
+	case memo.CodeSetName:
+		var memoSetName = db.MemoSetName{
+			TxHash:     txn.Hash,
+			PkHash:     inputAddress.ScriptAddress(),
+			PkScript:   out.PkScript,
+			ParentHash: parentHash,
+			Address:    inputAddress.EncodeAddress(),
+			Name:       html.EscapeString(string(out.PkScript[5:])),
+			BlockId:    blockId,
+		}
+		err := memoSetName.Save()
+		if err != nil {
+			return jerr.Get("error saving memo_set_name", err)
+		}
+	}
+	return nil
+}
+
+func updateMemo(txn *db.Transaction, out *db.TransactionOut, block *db.Block) error {
+	memoTest, err := db.GetMemoTest(txn.Hash)
+	if err != nil {
+		return jerr.Get("error getting memo_test", err)
+	}
+	if block == nil || memoTest.BlockId != 0 {
+		// Nothing to update
+		return nil
+	}
+	memoTest.BlockId = block.Id
+	err = memoTest.Save()
+	if err != nil {
+		return jerr.Get("error saving memo_test", err)
+	}
+	switch out.PkScript[3] {
+	case memo.CodePost:
+		memoPost, err := db.GetMemoPost(txn.Hash)
+		if err != nil {
+			return jerr.Get("error getting memo_post", err)
+		}
+		memoPost.BlockId = block.Id
+		err = memoPost.Save()
+		if err != nil {
+			return jerr.Get("error saving memo_post", err)
+		}
+	case memo.CodeSetName:
+		memoSetName, err := db.GetMemoSetName(txn.Hash)
+		if err != nil {
+			return jerr.Get("error getting memo_set_name", err)
+		}
+		memoSetName.BlockId = block.Id
+		err = memoSetName.Save()
+		if err != nil {
+			return jerr.Get("error saving memo_set_name", err)
 		}
 	}
 	return nil
--- app/bitcoin/transaction/save.go
+++ app/bitcoin/transaction/save.go
@@ -15,69 +15,104 @@ func SaveTransaction(msg *wire.MsgTx, block *db.Block) error {
 		return jerr.Get("error getting transaction from db", err)
 	}
 	if txn != nil {
-		if block == nil {
-			// Nothing to update
-			return nil
-		}
-		txn.BlockId = block.Id
-		err := txn.Save()
+		err = updateTxn(txn, block)
 		if err != nil {
-			return jerr.Get("error saving updated transaction", err)
+			return jerr.Get("error updating transaction", err)
 		}
+	} else {
+		txn, err = db.ConvertMsgToTransaction(msg)
+		if err != nil {
+			return jerr.Get("error converting message to transaction", err)
+		}
+		err = newTxn(txn, block)
+		if err != nil {
+			return jerr.Get("error saving new transaction", err)
+		}
+	}
+	memoOutput, err := GetMemoOutputIfExists(txn)
+	if err != nil {
+		return jerr.Get("error getting memo output", err)
+	}
+	if memoOutput != nil {
+		err = SaveMemo(txn, memoOutput, block)
+		if err != nil {
+			return jerr.Get("error saving memos", err)
+		}
+	}
+	return nil
+}
+
+func updateTxn(txn *db.Transaction, block *db.Block) error {
+	if block == nil {
+		// Nothing to update
 		return nil
 	}
-
-	txn, err = db.ConvertMsgToTransaction(msg)
+	txn.BlockId = block.Id
+	err := txn.Save()
 	if err != nil {
-		return jerr.Get("error converting message to transaction", err)
+		return jerr.Get("error saving updated transaction", err)
 	}
+	return nil
+}
 
-	fmt.Printf("Found new txn: %s\n", txn.GetChainHash().String())
-
+func newTxn(txn *db.Transaction, block *db.Block) error {
+	var blockId uint
 	if block != nil {
-		txn.BlockId = block.Id
+		blockId = block.Id
 	}
-
+	txn.BlockId = blockId
+	fmt.Printf("Found new txn: %s, block id: %d\n", txn.GetChainHash().String(), blockId)
 	for _, in := range txn.TxIn {
-		existingOutputs, err := db.GetTransactionOutputsForPkHash(in.KeyPkHash)
+		err := updateExistingOutputs(in)
 		if err != nil {
-			return jerr.Get("error getting existing outputs", err)
-		}
-		for _, existingTxOut := range existingOutputs {
-			if ! bytes.Equal(in.PreviousOutPointHash, existingTxOut.Transaction.Hash) {
-				continue
-			}
-			if uint32(existingTxOut.Index) == in.PreviousOutPointIndex {
-				existingTxOut.TxnInHashString = in.HashString
-				err := existingTxOut.Save()
-				if err != nil {
-					return jerr.Get("error saving existing transaction output", err)
-				}
-			}
+			return jerr.Get("error updating existing outputs", err)
 		}
 	}
 
 	for _, out := range txn.TxOut {
-		existingInputs, err := db.GetTransactionInputsForPkHash(out.KeyPkHash)
+		err := updateExistingInputs(out, txn.Hash)
 		if err != nil {
-			return jerr.Get("error getting existing inputs", err)
-		}
-		for _, existingTxIn := range existingInputs {
-			if bytes.Equal(txn.Hash, existingTxIn.PreviousOutPointHash) && out.Index == existingTxIn.PreviousOutPointIndex {
-				out.TxnInHashString = existingTxIn.HashString
-			}
+			return jerr.Get("error updating existing inputs", err)
 		}
 	}
 
-	err = txn.Save()
+	err := txn.Save()
 	if err != nil {
 		return jerr.Get("error saving txn", err)
 	}
 
-	err = FindAndSaveMemos(txn, block)
-	if err != nil {
-		return jerr.Get("error finding and saving memos", err)
-	}
+	return nil
+}
 
+func updateExistingOutputs(in *db.TransactionIn) error {
+	existingOutputs, err := db.GetTransactionOutputsForPkHash(in.KeyPkHash)
+	if err != nil {
+		return jerr.Get("error getting existing outputs", err)
+	}
+	for _, existingTxOut := range existingOutputs {
+		if ! bytes.Equal(in.PreviousOutPointHash, existingTxOut.Transaction.Hash) {
+			continue
+		}
+		if uint32(existingTxOut.Index) == in.PreviousOutPointIndex {
+			existingTxOut.TxnInHashString = in.HashString
+			err := existingTxOut.Save()
+			if err != nil {
+				return jerr.Get("error saving existing transaction output", err)
+			}
+		}
+	}
+	return nil
+}
+
+func updateExistingInputs(out *db.TransactionOut, txHash []byte) error {
+	existingInputs, err := db.GetTransactionInputsForPkHash(out.KeyPkHash)
+	if err != nil {
+		return jerr.Get("error getting existing inputs", err)
+	}
+	for _, existingTxIn := range existingInputs {
+		if bytes.Equal(txHash, existingTxIn.PreviousOutPointHash) && out.Index == existingTxIn.PreviousOutPointIndex {
+			out.TxnInHashString = existingTxIn.HashString
+		}
+	}
 	return nil
 }
