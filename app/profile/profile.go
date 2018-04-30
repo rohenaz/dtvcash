67807be9afacdd1195a63a1ba1ed5b5ececbfdd1
Jason Chavannes <jason.chavannes@gmail.com>
2018-04-30 16:55:15 -0700

    Basic reputation calculation

--- app/profile/profile.go
+++ app/profile/profile.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"git.jasonc.me/main/bitcoin/bitcoin/wallet"
 	"git.jasonc.me/main/memo/app/db"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcutil"
 	"github.com/cpacia/bchutil"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/jchavannes/jgo/jerr"
 )
 
@@ -24,6 +24,7 @@ type Profile struct {
 	FollowingCount uint
 	Followers      []*Follower
 	Following      []*Follower
+	Reputation     *Reputation
 }
 
 func (p Profile) IsSelf() bool {
@@ -145,6 +146,15 @@ func (p *Profile) SetFollowingCount() error {
 		return jerr.Get("error getting following count for hash", err)
 	}
 	p.FollowingCount = cnt
+	return nil
+}
+
+func (p *Profile) SetReputation() error {
+	reputation, err := GetReputation(p.SelfPkHash, p.PkHash)
+	if err != nil {
+		return jerr.Get("error getting reputation", err)
+	}
+	p.Reputation = reputation
 	return nil
 }
 
---
+++ app/profile/reputation.go
@@ -0,0 +1,59 @@
+package profile
+
+import (
+	"bytes"
+	"fmt"
+	"git.jasonc.me/main/memo/app/db"
+	"github.com/jchavannes/jgo/jerr"
+)
+
+type Reputation struct {
+	TrustedFollowers int
+	TotalFollowing   int
+}
+
+func (r Reputation) GetPercentString() string {
+	return fmt.Sprintf("%.2f", float32(r.TrustedFollowers)/float32(r.TotalFollowing)*100)
+}
+
+func GetReputation(selfPkHash []byte, pkHash []byte) (*Reputation, error) {
+	trustedUsers, err := db.GetFollowersForPkHash(selfPkHash)
+	if err != nil {
+		return nil, jerr.Get("error getting trustedUsers", err)
+	}
+	followersToCheck, err := db.GetFollowingForPkHash(pkHash)
+	if err != nil {
+		return nil, jerr.Get("error getting followersToCheck", err)
+	}
+	var deDupedTrustedUsers []*db.MemoFollow
+TrustedFollowersDeDupeLoop:
+	for _, trustedUser := range trustedUsers {
+		for _, deDupedTrustedUser := range deDupedTrustedUsers {
+			if bytes.Equal(deDupedTrustedUser.FollowPkHash, trustedUser.FollowPkHash) {
+				continue TrustedFollowersDeDupeLoop
+			}
+		}
+		deDupedTrustedUsers = append(deDupedTrustedUsers, trustedUser)
+	}
+	for _, deDupedTrustedUser := range deDupedTrustedUsers {
+		name, err := db.GetNameForPkHash(deDupedTrustedUser.FollowPkHash)
+		if err != nil {
+			return nil, jerr.Get("error getting name for pk hash", err)
+		}
+		fmt.Printf("deDupedTrustedUser name: %s\n", name.Name)
+	}
+	var trustedFollowers []*db.MemoFollow
+TrustedFollowersLoop:
+	for _, trustedUser := range deDupedTrustedUsers {
+		for _, followerToCheck := range followersToCheck {
+			if bytes.Equal(followerToCheck.PkHash, trustedUser.FollowPkHash) {
+				trustedFollowers = append(trustedFollowers, followerToCheck)
+				continue TrustedFollowersLoop
+			}
+		}
+	}
+	return &Reputation{
+		TrustedFollowers: len(trustedFollowers),
+		TotalFollowing:   len(deDupedTrustedUsers),
+	}, nil
+}
