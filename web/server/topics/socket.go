8a1598a5539c2d43049312bfe84abec1ffa07de4
Jason Chavannes <jason.chavannes@gmail.com>
2018-05-06 09:04:00 -0700

    Fix socket

--- web/server/topics/socket.go
+++ web/server/topics/socket.go
@@ -1,27 +1,31 @@
 package topics
 
 import (
-	"github.com/memocash/memo/app/html-parser"
-	"github.com/memocash/memo/app/res"
-	"github.com/memocash/memo/app/watcher"
 	"github.com/jchavannes/jgo/jerr"
 	"github.com/jchavannes/jgo/web"
+	"github.com/memocash/memo/app/res"
+	"github.com/memocash/memo/app/watcher"
 	"net/http"
+	"net/url"
 )
 
 var socketRoute = web.Route{
 	Pattern: res.UrlTopicsSocket,
 	Handler: func(r *web.Response) {
 		topicRaw := r.Request.GetUrlParameter("topic")
+		unescaped, err := url.QueryUnescape(topicRaw)
+		if err != nil {
+			r.Error(jerr.Get("error unescaping topic", err), http.StatusUnprocessableEntity)
+			return
+		}
 		lastPostId := r.Request.GetUrlParameterUInt("lastPostId")
 		lastLikeId := r.Request.GetUrlParameterUInt("lastLikeId")
-		topic := html_parser.EscapeWithEmojis(topicRaw)
 		socket, err := r.GetWebSocket()
 		if err != nil {
 			r.Error(jerr.Get("error getting socket", err), http.StatusUnprocessableEntity)
 			return
 		}
-		err = watcher.RegisterSocket(socket, topic, lastPostId, lastLikeId)
+		err = watcher.RegisterSocket(socket, unescaped, lastPostId, lastLikeId)
 		if err != nil {
 			r.Error(jerr.Get("error writing to socket", err), http.StatusInternalServerError)
 			return
