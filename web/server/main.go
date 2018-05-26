78dbce87312b97d25b37da927e981063d64d4d17
Eric Winchell <eric.winchell@gmail.com>
2018-05-25 18:45:02 -0700

    MEMO-153 french language update

--- web/server/main.go
+++ web/server/main.go
@@ -120,7 +120,7 @@ func notFoundHandler(r *web.Response) {
 }
 
 func isValidLang(lang string) bool {
-	for _, item := range []string{"en-US", "es-LA", "zh-CN", "ja-JP"} {
+	for _, item := range []string{"en-US", "es-LA", "zh-CN", "ja-JP", "fr-FR"} {
 		if item == lang {
 			return true
 		}
