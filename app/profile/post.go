a623d257bcc30d1412097519091f15ac6b47a115
Luke <rohenaz@gmail.com>
2018-05-26 00:06:45 -0400

    add the real video

--- app/profile/post.go
+++ app/profile/post.go
@@ -59,6 +59,12 @@ func (p Post) GetMessage() string {
 	return msg
 }
 
+func (p Post) MagnetLink() string {
+	//'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
+	var re = regexp.MustCompile(`(magnet:\?xt=urn:btih:[a-z0-9]{40})`)
+	return re.FindString(p.Memo.Message)
+}
+
 func (p Post) HasMagnet() bool {
 	var re = regexp.MustCompile(`(magnet:\?xt=urn:btih:[a-z0-9]{40})`)
 	return re.MatchString(p.Memo.Message)
