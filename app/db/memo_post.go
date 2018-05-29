bedb24896c63d7503eee080045b5c17850b23e66
Luke <rohenaz@gmail.com>
2018-05-29 00:12:18 -0400

    channels

--- app/db/memo_post.go
+++ app/db/memo_post.go
@@ -488,9 +488,6 @@ func GetPostsForTopic(topic string, offset uint) ([]*MemoPost, error) {
 	if result.Error != nil {
 		return nil, jerr.Get("error getting memo posts", result.Error)
 	}
-	for i, j := 0, len(memoPosts)-1; i < j; i, j = i+1, j-1 {
-		memoPosts[i], memoPosts[j] = memoPosts[j], memoPosts[i]
-	}
 	return memoPosts, nil
 }
 
@@ -514,8 +511,5 @@ func GetOlderPostsForTopic(topic string, firstPostId uint) ([]*MemoPost, error) 
 	if result.Error != nil {
 		return nil, jerr.Get("error getting memo posts", result.Error)
 	}
-	// for i, j := 0, len(memoPosts)-1; i < j; i, j = i+1, j-1 {
-	// 	memoPosts[i], memoPosts[j] = memoPosts[j], memoPosts[i]
-	// }
 	return memoPosts, nil
 }
