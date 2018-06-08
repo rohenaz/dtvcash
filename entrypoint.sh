6119a0ed23c18adcc3e5d5c11894441f10f078de
Luke <rohenaz@gmail.com>
2018-06-08 16:24:39 -0400

    nothingness

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
-set -e
+# set -e
 
-if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1" == "test_bitcoin" ]]; then
-	mkdir -p "$BCH_DATA"
+# if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1" == "test_bitcoin" ]]; then
+# 	mkdir -p "$BCH_DATA"
 
-	if [[ ! -s "$BCH_DATA/bitcoin.conf" ]]; then
-		cat <<-EOF > "$BCH_DATA/bitcoin.conf"
-		printtoconsole=1
-		rpcallowip=::/0
-		rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
-		rpcuser=${BITCOIN_RPC_USER:-bitcoin}
-		EOF
-		chown bitcoin:bitcoin "$BCH_DATA/bitcoin.conf"
-	fi
+# 	if [[ ! -s "$BCH_DATA/bitcoin.conf" ]]; then
+# 		cat <<-EOF > "$BCH_DATA/bitcoin.conf"
+# 		printtoconsole=1
+# 		rpcallowip=::/0
+# 		rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
+# 		rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+# 		EOF
+# 		chown bitcoin:bitcoin "$BCH_DATA/bitcoin.conf"
+# 	fi
 
-	# ensure correct ownership and linking of data directory
-	# we do not update group ownership here, in case users want to mount
-	# a host directory and still retain access to it
-	chown -R bitcoin "$BCH_DATA"
-	ln -sfn "$BCH_DATA" /home/bitcoin/.bitcoin
-	chown -h bitcoin:bitcoin /home/bitcoin/.bitcoin
+# 	# ensure correct ownership and linking of data directory
+# 	# we do not update group ownership here, in case users want to mount
+# 	# a host directory and still retain access to it
+# 	chown -R bitcoin "$BCH_DATA"
+# 	ln -sfn "$BCH_DATA" /home/bitcoin/.bitcoin
+# 	chown -h bitcoin:bitcoin /home/bitcoin/.bitcoin
 
-	exec gosu bitcoin "$@"
-fi
+# 	exec gosu bitcoin "$@"
+# fi
+bitcoind --daemon&
 
-exec "$@"
+# exec "$@"
 
 go build /go/src/github.com/rohenaz/dtvcash/
 cd /go/src/github.com/rohenaz/dtvcash/ && ./dtvcash web&
